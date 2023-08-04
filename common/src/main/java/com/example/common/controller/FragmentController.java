e440e6d4b805f3a5289402cf346162f369805714
Dirk <dirkdeyne@gmail.com>
2023-08-04 16:46:03 +0200

    normal action work when using internal fragments only

---
+++ common/src/main/java/com/example/common/controller/FragmentController.java
@@ -0,0 +1,7 @@
+package com.example.common.controller;
+
+import io.getmedusa.medusa.core.annotation.UIEventPage;
+
+@UIEventPage(path = "/fragment", file = "/pages/fragment-count")
+public class FragmentController {
+}
